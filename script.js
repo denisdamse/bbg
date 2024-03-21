4acad6094d5d4363c5dbe9725dc010699d7725c2
Timotei Adam <timotei.adam@emanuel.ro>
2024-03-21 12:39:17 +0200

    smileyFaceTimmy

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function setup()
 {
-    createCanvas(800,600) 
+    createCanvas(800,600);
 }
 function draw()
 {
+    background("green");
 
+    smileyFaceTimmy();
+}
+
+function smileyFaceTimmy() {
+//BODY
+fill("yellow");
+stroke("black");
+circle(400, 300, 50);
+
+//EYES
+fill("white");
+stroke("brown");
+ellipse(390, 290, 10, 5);
+ellipse(410, 290, 10, 5);
+
+//EYES PUPILS
+stroke("black");
+fill("black");
+ellipse(390, 290, 2);
+ellipse(410, 290, 2);
+
+//MOUTH
+stroke("red");
+fill("gray");
+arc(400, 310, 20, 10, 0, PI);
 }

26128d19b4fc41dbf43965e3c830cb400bdfa454
roberta7724 <roberta.bara@emanuel.ro>
2024-03-21 12:39:34 +0200

    smileyFaceBeta

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ function setup()
 {
     createCanvas(800,600) 
 }
-function draw()
+function draw(){
+    background ("green");
+    
+   smileyFaceBeta()
+
+
+}
+
+function smileyFaceBeta()
 {
+ //BODY
+ fill("white");
+ stroke("black");
+ circle (400, 300, 50);
+ 
+
+ //EYES
+ // circle(390, 290, 5);
+// circle(410,290,5);
+stroke("brown");
+ellipse(390,290,10,5);
+ellipse(410, 290,10,5);
+
+//PUPILS
+//stroke("black");
+fill("black");
+circle(390,290,2);
+circle(410,290,2);
+
+//MOUTH
+arc(400, 310, 20, 10, 0, Math.PI);
+
 
 }
